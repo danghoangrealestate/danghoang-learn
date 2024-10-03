f1f81c931a8d7a33427aaff693cffbfcc6c42ed9
Dang Hoang <thedreamneverend@gmail.com>
2024-10-03 15:22:27 +0700

    init commit

---
+++ Untitled-1.py
@@ -0,0 +1,24 @@
+#print (1, 2, 3, 4, 5, sep = ' % ', end = '\n') #newline
+#print(6)
+
+#print("\thello\t") #tab#
+
+x = 5
+print(type(x))
+x=5.6
+print(type(x))
+
+x = 3
+y = 4
+print (x+y)
+print (x/y) #in ra số thực
+print (x//y) # chỉ in ra phần nguyên
+
+x=3
+y=4.5
+print (x//y) # result: 0.0
+
+a = 2
+b= 3
+print (a ** b) # a luy thua b --> 2^3 = 8
+
